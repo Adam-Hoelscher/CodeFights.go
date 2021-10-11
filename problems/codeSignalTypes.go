ad89f1f5833f48f1c1408ec6ea871eb1da2b8cff
Adam Hoelscher <adam.hoelscher@gmail.com>
2021-10-11 07:46:36 -0700

    add binary tree struct

--- problems/codeSignalTypes.go
+++ problems/codeSignalTypes.go
@@ -5,3 +5,10 @@ type ListNode struct {
 	Value interface{}
 	Next  *ListNode
 }
+
+// Binary trees are already defined with this interface:
+type Tree struct {
+	Value interface{}
+	Left  *Tree
+	Right *Tree
+}
