09dadb9a903152705d9e61428f7149a996dd5f4b
Adam Hoelscher <adam.hoelscher@gmail.com>
2021-11-21 18:51:12 -0800

    priority queue in main

--- main.go
+++ main.go
@@ -1,16 +1,85 @@
+// This example demonstrates a priority queue built using the heap interface.
 package main
 
 import (
+	"container/heap"
 	"fmt"
 )
 
+type queueItem struct {
+	priority int
+	value    interface{}
+}
+
+type queueHeap []*queueItem
+
+func (pq *queueHeap) Push(x interface{}) {
+	item := x.(*queueItem)
+	*pq = append(*pq, item)
+}
+
+func (pq *queueHeap) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil
+	*pq = old[0 : n-1]
+	return item.value
+}
+
+func (pq queueHeap) Len() int { return len(pq) }
+
+func (pq queueHeap) Less(i, j int) bool {
+	return pq[i].priority < pq[j].priority
+}
+
+func (pq queueHeap) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+type PriorityQueue struct {
+	heap queueHeap
+}
+
+func (pq PriorityQueue) Empty() bool { return pq.Len() == 0 }
+
+func (pq PriorityQueue) Len() int { return len(pq.heap) }
+
+func (pq *PriorityQueue) Push(priortiy int, value interface{}) queueItem {
+	item := queueItem{priority: priortiy, value: value}
+	heap.Push(&pq.heap, &item)
+	return item
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	return heap.Pop(&pq.heap)
+}
+
 func main() {
-	q := []int{1, 2, 3}
-	for _, v := range q {
-		fmt.Println(v)
-		if v == 2 {
-			q = append(q, 4)
-		}
+
+	pq := PriorityQueue{}
+
+	stuff := [][2]interface{}{
+		{7, "a"},
+		{0, "e"},
+		{1, "g"},
+		{2, "b"},
+		{3, "c"},
+		{6, "d"},
+		{4, "f"},
+		{5, "h"},
 	}
-	fmt.Println(q)
+
+	for _, i := range stuff {
+
+		p := i[0].(int)
+		v := i[1]
+
+		fmt.Println("Add", pq.Push(p, v))
+	}
+
+	fmt.Println()
+	for !pq.Empty() {
+		fmt.Println("Pop", pq.Pop())
+	}
 }
